04a6e9e78b714a33184c9e7726afd3aa48423831
miu200521358 <garnet200521358@gmail.com>
2022-10-10 23:51:56 +0900

    AlphaPose 3D

--- alphapose/utils/writer_smpl.py
+++ alphapose/utils/writer_smpl.py
@@ -1,29 +1,21 @@
 import os
 import time
-from threading import Thread
 from queue import Queue
+from threading import Thread
 
 import cv2
 import numpy as np
 import torch
 import torch.multiprocessing as mp
-
 from alphapose.utils.pPose_nms import pose_nms, write_json
 
-DEFAULT_VIDEO_SAVE_OPT = {
-    'savepath': 'examples/res/1.mp4',
-    'fourcc': cv2.VideoWriter_fourcc(*'mp4v'),
-    'fps': 25,
-    'frameSize': (640, 480)
-}
+DEFAULT_VIDEO_SAVE_OPT = {"savepath": "examples/res/1.mp4", "fourcc": cv2.VideoWriter_fourcc(*"mp4v"), "fps": 25, "frameSize": (640, 480)}
 
 EVAL_JOINTS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 
-class DataWriterSMPL():
-    def __init__(self, cfg, opt, save_video=False,
-                 video_save_opt=DEFAULT_VIDEO_SAVE_OPT,
-                 queueSize=1024):
+class DataWriterSMPL:
+    def __init__(self, cfg, opt, save_video=False, video_save_opt=DEFAULT_VIDEO_SAVE_OPT, queueSize=1024):
         self.cfg = cfg
         self.opt = opt
         self.video_save_opt = video_save_opt
@@ -39,18 +31,19 @@ class DataWriterSMPL():
             self.result_queue = mp.Queue(maxsize=queueSize)
 
         if opt.save_img:
-            if not os.path.exists(opt.outputpath + '/vis'):
-                os.mkdir(opt.outputpath + '/vis')
+            if not os.path.exists(opt.outputpath + "/vis"):
+                os.mkdir(opt.outputpath + "/vis")
 
         if opt.pose_flow:
             from trackers.PoseFlow.poseflow_infer import PoseFlowWrapper
-            self.pose_flow_wrapper = PoseFlowWrapper(save_path=os.path.join(opt.outputpath, 'poseflow'))
+
+            self.pose_flow_wrapper = PoseFlowWrapper(save_path=os.path.join(opt.outputpath, "poseflow"))
 
         if self.opt.save_img or self.save_video or self.opt.vis:
             # loss_type = self.cfg.DATA_PRESET.get('LOSS_TYPE', 'MSELoss')
             self.vis_thres = [0.01] * 29
 
-        self.use_heatmap_loss = (self.cfg.DATA_PRESET.get('LOSS_TYPE', 'MSELoss') == 'MSELoss')
+        self.use_heatmap_loss = self.cfg.DATA_PRESET.get("LOSS_TYPE", "MSELoss") == "MSELoss"
 
     def start_worker(self, target):
         if self.opt.sp:
@@ -68,29 +61,28 @@ class DataWriterSMPL():
 
     def update(self):
         final_result = []
-        norm_type = self.cfg.LOSS.get('NORM_TYPE', None)
+        norm_type = self.cfg.LOSS.get("NORM_TYPE", None)
         hm_size = self.cfg.DATA_PRESET.HEATMAP_SIZE
         if self.save_video:
             # initialize the file video stream, adapt ouput video resolution to original video
-            stream = cv2.VideoWriter(*[self.video_save_opt[k] for k in ['savepath', 'fourcc', 'fps', 'frameSize']])
+            stream = cv2.VideoWriter(*[self.video_save_opt[k] for k in ["savepath", "fourcc", "fps", "frameSize"]])
             if not stream.isOpened():
                 print("Try to use other video encoders...")
-                ext = self.video_save_opt['savepath'].split('.')[-1]
+                ext = self.video_save_opt["savepath"].split(".")[-1]
                 fourcc, _ext = self.recognize_video_ext(ext)
-                self.video_save_opt['fourcc'] = fourcc
-                self.video_save_opt['savepath'] = self.video_save_opt['savepath'][:-4] + _ext
+                self.video_save_opt["fourcc"] = fourcc
+                self.video_save_opt["savepath"] = self.video_save_opt["savepath"][:-4] + _ext
                 if self.opt.show_skeleton:
-                    ori_w, ori_h = self.video_save_opt['frameSize']
+                    ori_w, ori_h = self.video_save_opt["frameSize"]
                     skeleton_img_h, skeleton_img_w = 694, 693
                     new_h, new_w = max(skeleton_img_h, ori_h), skeleton_img_w + ori_w
-                    self.video_save_opt['frameSize'] = new_w, new_h
-                stream = cv2.VideoWriter(*[self.video_save_opt[k] for k in ['savepath', 'fourcc', 'fps', 'frameSize']])
-            assert stream.isOpened(), 'Cannot open video for writing'
+                    self.video_save_opt["frameSize"] = new_w, new_h
+                stream = cv2.VideoWriter(*[self.video_save_opt[k] for k in ["savepath", "fourcc", "fps", "frameSize"]])
+            assert stream.isOpened(), "Cannot open video for writing"
         # keep looping infinitelyd
         while True:
             # ensure the queue is not empty and get item
-            (boxes, scores, ids, smpl_output, cropped_boxes,
-             orig_img, im_name) = self.wait_and_get(self.result_queue)
+            (boxes, scores, ids, smpl_output, cropped_boxes, orig_img, im_name) = self.wait_and_get(self.result_queue)
             if orig_img is None:
                 # if the thread indicator variable is set (img is None), stop the thread
                 if self.save_video:
@@ -105,15 +97,15 @@ class DataWriterSMPL():
                     self.write_image(orig_img, im_name, stream=stream if self.save_video else None)
             else:
                 # location prediction (n, kp, 2) | score prediction (n, kp, 1)
-                uv_29 = smpl_output['pred_uvd_jts'].reshape(-1, 29, 3)[:, :, :2].cpu()
-                pred_xyz_jts_24 = smpl_output['pred_xyz_jts_24'].reshape(-1, 24, 3).cpu()
+                uv_29 = smpl_output["pred_uvd_jts"].reshape(-1, 29, 3)[:, :, :2].cpu()
+                pred_xyz_jts_24 = smpl_output["pred_xyz_jts_24"].reshape(-1, 24, 3).cpu()
 
                 pose_coords = uv_29 * (cropped_boxes[:, [2], None] - cropped_boxes[:, [0], None])
                 pose_coords[:, :, 0] = pose_coords[:, :, 0] + (cropped_boxes[:, [0]] + cropped_boxes[:, [2]]) / 2
                 pose_coords[:, :, 1] = pose_coords[:, :, 1] + (cropped_boxes[:, [1]] + cropped_boxes[:, [3]]) / 2
 
                 preds_img = pose_coords
-                preds_scores = 1 - smpl_output['maxvals'].reshape(-1, 29, 1)
+                preds_scores = 1 - smpl_output["maxvals"].reshape(-1, 29, 1)
                 # if not self.opt.pose_track:
                 #     boxes, scores, ids, preds_img, preds_scores, pick_ids = \
                 #         pose_nms(boxes, scores, ids, preds_img, preds_scores, self.opt.min_box_area, use_heatmap_loss=self.use_heatmap_loss)
@@ -124,35 +116,39 @@ class DataWriterSMPL():
                 for k in range(len(scores)):
                     _result.append(
                         {
-                            'keypoints': preds_img[k],
-                            'pred_xyz_jts': pred_xyz_jts_24[k],
-                            'kp_score': preds_scores[k],
-                            'proposal_score': torch.mean(preds_scores[k]) + scores[k] + 1.25 * max(preds_scores[k]),
-                            'bbox_score': scores[k].cpu(),
-                            'idx': int(ids[k]),
+                            "keypoints": preds_img[k],
+                            "pred_xyz_jts": pred_xyz_jts_24[k],
+                            "kp_score": preds_scores[k],
+                            "proposal_score": torch.mean(preds_scores[k]) + scores[k] + 1.25 * max(preds_scores[k]),
+                            "bbox_score": scores[k].cpu(),
+                            "idx": int(ids[k]),
                             # xywh
-                            'box': [boxes[k][0], boxes[k][1], boxes[k][2]-boxes[k][0],boxes[k][3]-boxes[k][1]],
+                            "box": [boxes[k][0], boxes[k][1], boxes[k][2] - boxes[k][0], boxes[k][3] - boxes[k][1]],
                             # xywh
-                            'crop_box': [cropped_boxes[k][0], cropped_boxes[k][1], cropped_boxes[k][2]-cropped_boxes[k][0],cropped_boxes[k][3]-cropped_boxes[k][1]] 
+                            "crop_box": [
+                                cropped_boxes[k][0],
+                                cropped_boxes[k][1],
+                                cropped_boxes[k][2] - cropped_boxes[k][0],
+                                cropped_boxes[k][3] - cropped_boxes[k][1],
+                            ],
                         }
                     )
 
-                result = {
-                    'imgname': im_name,
-                    'result': _result
-                }
+                result = {"imgname": im_name, "result": _result}
 
                 if self.opt.pose_flow:
                     poseflow_result = self.pose_flow_wrapper.step(orig_img, result)
                     for i in range(len(poseflow_result)):
-                        result['result'][i]['idx'] = poseflow_result[i]['idx']
+                        result["result"][i]["idx"] = poseflow_result[i]["idx"]
 
                 final_result.append(result)
                 if self.opt.save_img or self.save_video or self.opt.vis:
-                    from alphapose.utils.vis import vis_frame_smpl
-                    img = vis_frame_smpl(orig_img, result, smpl_output, self.opt, self.vis_thres)
+                    # from alphapose.utils.vis import vis_frame_smpl
+                    # img = vis_frame_smpl(orig_img, result, smpl_output, self.opt, self.vis_thres)
+                    img = orig_img
                     if self.opt.show_skeleton:
                         from alphapose.utils.vis import vis_frame_skeleton
+
                         img = vis_frame_skeleton(img, result, smpl_output, self.opt, self.vis_thres)
                     self.write_image(img, im_name, stream=stream if self.save_video else None)
 
@@ -161,7 +157,7 @@ class DataWriterSMPL():
             cv2.imshow("AlphaPose Demo", img)
             cv2.waitKey(30)
         if self.opt.save_img:
-            cv2.imwrite(os.path.join(self.opt.outputpath, 'vis', im_name), img)
+            cv2.imwrite(os.path.join(self.opt.outputpath, "vis", im_name), img)
         if self.save_video:
             stream.write(img)
 
@@ -194,7 +190,7 @@ class DataWriterSMPL():
 
     def clear_queues(self):
         self.clear(self.result_queue)
-        
+
     def clear(self, queue):
         while not queue.empty():
             queue.get()
@@ -204,13 +200,13 @@ class DataWriterSMPL():
         print(self.final_result)
         return self.final_result
 
-    def recognize_video_ext(self, ext=''):
-        if ext == 'mp4':
-            return cv2.VideoWriter_fourcc(*'mp4v'), '.' + ext
-        elif ext == 'avi':
-            return cv2.VideoWriter_fourcc(*'XVID'), '.' + ext
-        elif ext == 'mov':
-            return cv2.VideoWriter_fourcc(*'XVID'), '.' + ext
+    def recognize_video_ext(self, ext=""):
+        if ext == "mp4":
+            return cv2.VideoWriter_fourcc(*"mp4v"), "." + ext
+        elif ext == "avi":
+            return cv2.VideoWriter_fourcc(*"XVID"), "." + ext
+        elif ext == "mov":
+            return cv2.VideoWriter_fourcc(*"XVID"), "." + ext
         else:
             print("Unknow video format {}, will use .mp4 instead of it".format(ext))
-            return cv2.VideoWriter_fourcc(*'mp4v'), '.mp4'
+            return cv2.VideoWriter_fourcc(*"mp4v"), ".mp4"
